8f9722f22d8dbe87b4b9fe78ff1e1bbf4057b637
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-06-19 00:50:54 +0300

    date conversion
    date conversion

--- HackerRankCSharp/HackerRankCSharp/Program.cs
+++ HackerRankCSharp/HackerRankCSharp/Program.cs
@@ -6,14 +6,31 @@
         //StringToInteger(S);
         //PrintEvenOddIndices("Hacker");
 
-        int n, p;
-        n=int.Parse(Console.ReadLine());
-        p = int.Parse(Console.ReadLine());
-      
-            Calculator calculator = new Calculator();
-            int result = calculator.power(n, p);
-            Console.WriteLine(result);
-       
+        //int n, p;
+        //n=int.Parse(Console.ReadLine());
+        //p = int.Parse(Console.ReadLine());
+
+        //    Calculator calculator = new Calculator();
+        //    int result = calculator.power(n, p);
+        //    Console.WriteLine(result);
+
+        //string date = "07:05:45PM";
+
+        //if (date.EndsWith("PM"))
+        //{
+        //    string hourString = date.Substring(0, 2); // Extract the hour part (e.g., "07")
+        //    int hour = int.Parse(hourString); // Convert the hour string to an integer
+        //    hour += 12;
+        //    string time24HourFormat = hour.ToString().PadLeft(2, '0') + date.Substring(2, 6); // Create the time string in 24-hour format
+
+        //    Console.WriteLine(time24HourFormat);
+        //}
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+        Console.WriteLine(result);  
+
 
     }
     public static void StringToInteger(string S)
@@ -52,7 +69,7 @@ public class Calculator
 {
     public int power(int n, int p)
     {
-        if (n < 0 && p < 0)
+        if (n < 0 || p < 0)
         {
             throw new Exception("n and p should be non-negative");
 
@@ -62,5 +79,29 @@ public class Calculator
 
         
     }
+}
+class Result
+{
+
+    public static string timeConversion(string s)
+    {
+        string hourString = s.Substring(0, 2);
+        int hour = int.Parse(hourString);
+
+        if (s.EndsWith("PM") && hour != 12)
+        {
+            hour += 12;
+        }
+        else if (s.EndsWith("AM") && hour == 12)
+        {
+            hour = 0;
+        }
+
+        string hour24Format = hour.ToString().PadLeft(2, '0');
+        string time24Format = hour24Format + s.Substring(2, 6);
+
+        return time24Format;
     }
+
+}
 
