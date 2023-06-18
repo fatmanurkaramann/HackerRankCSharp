15c3b41ebb70af3a96ad9b90bff351b438071db8
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-06-18 20:13:25 +0300

    StringToInteger
    StringToInteger

--- HackerRankCSharp/HackerRankCSharp/Program.cs
+++ HackerRankCSharp/HackerRankCSharp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+       StringToInteger(S);
+    }
+    public static void StringToInteger(string S)
+    {
+      
+        int parsedInt;
+
+        if (int.TryParse(S, out parsedInt))
+        {
+            Console.WriteLine(parsedInt); // Print the parsed integer
+        }
+        else
+        {
+            Console.WriteLine("Bad String"); // Print "Bad String" if parsing fails
+        }
+    }
+}
+
+
