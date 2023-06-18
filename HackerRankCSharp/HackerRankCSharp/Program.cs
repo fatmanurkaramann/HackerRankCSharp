5310dde3fdb3b70e53ce59222a5e48f882318b88
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-06-19 00:12:47 +0300

    more exception

--- HackerRankCSharp/HackerRankCSharp/Program.cs
+++ HackerRankCSharp/HackerRankCSharp/Program.cs
@@ -2,8 +2,19 @@
 {
     public static void Main(string[] args)
     {
-        string S = Console.ReadLine();
-       StringToInteger(S);
+        //string S = Console.ReadLine();
+        //StringToInteger(S);
+        //PrintEvenOddIndices("Hacker");
+
+        int n, p;
+        n=int.Parse(Console.ReadLine());
+        p = int.Parse(Console.ReadLine());
+      
+            Calculator calculator = new Calculator();
+            int result = calculator.power(n, p);
+            Console.WriteLine(result);
+       
+
     }
     public static void StringToInteger(string S)
     {
@@ -12,13 +23,44 @@
 
         if (int.TryParse(S, out parsedInt))
         {
-            Console.WriteLine(parsedInt); // Print the parsed integer
+            Console.WriteLine(parsedInt);
         }
         else
         {
-            Console.WriteLine("Bad String"); // Print "Bad String" if parsing fails
+            Console.WriteLine("Bad String");
         }
     }
+    static void PrintEvenOddIndices(string S)
+    {
+        string evenChars = "";
+        string oddChars = "";
+        for (int i = 0; i < S.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                evenChars += S[i];
+            }
+            else
+            {
+                oddChars += S[i];
+            }
+        }
+        Console.WriteLine(evenChars + " " + oddChars);
+    }
 }
+public class Calculator
+{
+    public int power(int n, int p)
+    {
+        if (n < 0 && p < 0)
+        {
+            throw new Exception("n and p should be non-negative");
 
+        }
+         int sonuc = (int)Math.Pow(n, p);
+         return sonuc;
+
+        
+    }
+    }
 
